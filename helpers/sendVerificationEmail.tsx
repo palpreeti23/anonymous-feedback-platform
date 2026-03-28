4c1d2c24258f324c7643eb0de64a5f35443c4411
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2026-03-29 01:19:24 +0530

    fix: minor logic fix

--- helpers/sendVerificationEmail.tsx
+++ helpers/sendVerificationEmail.tsx
@@ -1,13 +1,25 @@
 import React from "react";
-import { resend } from "@/lib/resend";
+// import { resend } from "@/lib/resend";
 import VerificationEmail from "@/emails/verificationEmail";
 import { ApiResponse } from "@/types/ApiResponse";
+import { Resend } from "resend";
+import { success } from "zod";
 
 export async function sendVerificationEmail(
   username: string,
   email: string,
   verifyCode: string,
 ): Promise<ApiResponse> {
+  const apiKey = process.env.RESEND_API_KEY;
+
+  if (!apiKey) {
+    return {
+      success: false,
+      message: "Missing Resend API key",
+    };
+  }
+
+  const resend = new Resend(apiKey);
   try {
     await resend.emails.send({
       from: "Acme <onboarding@resend.dev>",
