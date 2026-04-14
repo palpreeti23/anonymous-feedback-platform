48046c9cc58d1173d295196d9930d02021171735
palpreeti23 <85545322+palpreeti23@users.noreply.github.com>
2026-04-14 15:26:56 +0530

    setup mongodb connection

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,5 @@
-import { handlers } from "./option";
+// import { handlers } from "./option";
+import { handlers } from "@/app/api/auth/[...nextauth]/option";
 
 export const GET = handlers.GET;
 export const POST = handlers.POST;
